56e0ece8af90fd6f3d337bd4c11680b1cbe1e195
Arun <arunreddy1820@gmail.com>
2025-09-26 13:10:36 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import './App.css'
+import Foodieland from "./components/foodieland"
+
+function App() {
+  
+
+  return (
+    <Foodieland/>
+  )
+}
+
+export default App
---
+++ src/components/foodieland/index.jsx
@@ -0,0 +1,14 @@
+import React from "react"
+
+export default function Foodieland() {
+    
+    const Navbar = () => {
+        return (
+            <div></div>
+        )
+    }
+    
+    return (
+        <Navbar/>
+    )
+}
